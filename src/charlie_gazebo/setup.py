78d3d00ada68c22c0e9506725e53ce370c656824
Alejandro Marin <alejandro.marin92@eia.edu.co>
2026-02-24 11:56:20 -0500

    correción setup.py

--- src/charlie_gazebo/setup.py
+++ src/charlie_gazebo/setup.py
@@ -15,7 +15,8 @@ setup(
         (os.path.join('share', package_name,'worlds'), glob('worlds/*.*')),
         (os.path.join('share', package_name,'config'), glob('config/*.*')),
         (os.path.join('share', package_name,'rviz_config'), glob('rviz_config/*.*')),
-                (os.path.join('share', package_name,'models'), glob('models/*.*')),
+        (os.path.join('share', package_name,'models'), glob('models/*.*')),
+        (os.path.join('share', package_name,'maps'), glob('maps/*.*')),
 
     ],
     install_requires=['setuptools'],
