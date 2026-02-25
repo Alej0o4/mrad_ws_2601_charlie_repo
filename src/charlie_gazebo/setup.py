9c5be3a4f751766c583f6fa12b3e8b6c0fa6cf49
ALEJOLIZ <ajejo.lizcano@gmail.com>
2026-02-25 15:38:14 -0500

    prueba de amcl y line segment

--- src/charlie_gazebo/setup.py
+++ src/charlie_gazebo/setup.py
@@ -17,6 +17,7 @@ setup(
         (os.path.join('share', package_name,'rviz_config'), glob('rviz_config/*.*')),
         (os.path.join('share', package_name,'models'), glob('models/*.*')),
         (os.path.join('share', package_name,'plot_juggler'), glob('plot_juggler/*.*')),
+        (os.path.join('share', package_name,'maps'), glob('maps/*.*')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
