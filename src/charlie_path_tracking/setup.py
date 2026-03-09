10940e9c7f0137ff9f45aa087131f7170a36d764
Alejandro Marin <alejandro.marin92@eia.edu.co>
2026-03-09 15:41:15 -0500

    Path tracking (Pure pursuit V1 )

---
+++ src/charlie_path_tracking/setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+
+package_name = 'charlie_path_tracking'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='alejo',
+    maintainer_email='alejandro.marin92@eia.edu.co',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'pure_pursuit_v1 = charlie_path_tracking.pure_pursuit_v1:main',
+        ],
+    },
+)
