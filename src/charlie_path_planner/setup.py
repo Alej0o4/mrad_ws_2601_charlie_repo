5938d99d194c5d77e72dc13b9a469f85916f478b
miconra <miguelangelconra@gmail.com>
2026-03-09 08:50:01 -0500

    Publicacion_diferentes_rutas

--- src/charlie_path_planner/setup.py
+++ src/charlie_path_planner/setup.py
@@ -31,6 +31,7 @@ setup(
             'dijkstra_node = charlie_path_planner.charlie_path_planner_dijkstra:main',
             'dijkstra_node_v3 = charlie_path_planner.charlie_path_planner_dijkstra_v3:main',
             'ara_star_node = charlie_path_planner.charlie_path_planner_ARA:main',
+            'waypoint_manager_node = charlie_path_planner.waypoint_manager:main',
         ],
     },
 )
