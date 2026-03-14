ad4e632c935ffa44a8769d9def16f0ad20a91ddb
Alejandro Marin <alejandro.marin92@eia.edu.co>
2026-03-14 12:01:13 -0500

    Cambio a mapa de carrera

--- src/charlie_bringup/launch/gz_spawn_diffdrive.launch.py
+++ src/charlie_bringup/launch/gz_spawn_diffdrive.launch.py
@@ -68,7 +68,8 @@ def generate_launch_description():
         arguments=[
             "-name", "diffbot",
             "-topic", "robot_description",
-            "-x", "0.0", "-y", "0.0", "-z", "0.5", "-Y", "0.0"
+            # "-x", "0.0", "-y", "0.0", "-z", "0.5", "-Y", "0.0"
+            "-x", "-18.0", "-y", "4.0", "-z", "0.5", "-Y", "1.57"
         ],
     )
 
@@ -145,7 +146,7 @@ def generate_launch_description():
         DeclareLaunchArgument(
             "world",
             # default_value=os.path.join(get_package_share_directory(gazebo_pkg_name), "worlds", "empty_world.sdf"),
-            default_value=os.path.join(get_package_share_directory(gazebo_pkg_name), "worlds", "walls_world2.sdf"),
+            default_value=os.path.join(get_package_share_directory(gazebo_pkg_name), "worlds", "RaceTrack.sdf"),
             description="Full path to world SDF file",
         ),
         DeclareLaunchArgument(
