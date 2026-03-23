d77d3a85a19ada401b92aa59f37f2cbcf4ccc389
Alejandro Marin <alejandro.marin92@eia.edu.co>
2026-03-23 10:16:51 -0500

    Ackerman initialize - gz spawn world choice

--- src/charlie_description/setup.py
+++ src/charlie_description/setup.py
@@ -15,6 +15,7 @@ setup(
         (os.path.join('share', package_name,'sensors'), glob('sensors/*.*')),
         (os.path.join('share', package_name,'diffdrive_urdf'), glob('diffdrive_urdf/*.*')),
         (os.path.join('share', package_name,'macros'), glob('macros/*.*')),
+        (os.path.join('share', package_name,'ackerman_urdf'), glob('ackerman_urdf/*.*')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
